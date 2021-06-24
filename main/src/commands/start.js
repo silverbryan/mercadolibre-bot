89e60e9a2cea806a853f129545f294b3bf121d55
silverbryan <soporte.g4a@gmail.com>
2021-06-24 10:20:36 -0300

    test 3

--- main/src/commands/start.js
+++ main/src/commands/start.js
@@ -1,6 +1,4 @@
 module.exports = ({ reply, from }) => {
     const { first_name, last_name } = from;
-    reply(`Hola, ${first_name} ${last_name} 👋🏻`);
-    reply(`Para comenzar solo tienes que escribir en cualquier chat de telegram @MercadolibreXbot.\n
-        Nota: pruebaEste paso solo se realiza una sola vez la proxima ya reconocera el comando automaticamente 😎`)
+    return reply(`Hola, ${first_name} ${last_name} 👋🏻`);
 }

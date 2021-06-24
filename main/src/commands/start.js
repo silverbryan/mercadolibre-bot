1ea74c34414d4aca832f21f0bf15eb69417e1d8e
silverbryan <soporte.g4a@gmail.com>
2021-06-24 10:47:54 -0300

    start command updated

--- main/src/commands/start.js
+++ main/src/commands/start.js
@@ -1,4 +1,6 @@
 module.exports = ({ reply, from }) => {
     const { first_name, last_name } = from;
-    return reply(`Hola, ${first_name} ${last_name} 👋🏻`);
+    reply(`Hola, ${first_name} ${last_name} 👋🏻`);
+    reply("Para empezar a usar escribe en el chat @mercadolibreXBot, asi como se muestra abajo 👇");
+    reply("!Perfecto! 👏🏻, Ahora podes usar el Bot en cualquier Chat, Grupo o Canal de Telegram sin limites 😎");
 }

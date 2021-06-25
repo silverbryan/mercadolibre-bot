0a584c222eba2aa3917673441fc823ad5648dba6
silverbryan <soporte.g4a@gmail.com>
2021-06-25 15:57:04 -0300

    update about

--- main/src/commands/about.js
+++ main/src/commands/about.js
@@ -1,1 +1,5 @@
-module.exports = ({ reply }) => reply("Este bot fue contruido por @silverbryan puedes encontrar mas informacion accediendo a su pagina oficial https://silverbryan.com.ar")
+module.exports = async({ reply }) => {
+    await reply('@mercadolibreXBot es un Bot No oficial de mercadolibre que fue creado por @silverbryan.\n👉 No recopilamos ningun tipo de informacion\n👉 Su uso es totalmente gratuito\n👉 Fue desarrollado solo con fines educativos.');
+    await reply("Podes ver el codigo fuente aqui 👇");
+    await reply("https://github.com/silverbryan/mercadolibre-bot");
+}
